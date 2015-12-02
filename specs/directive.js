348f4d42a71f4f5d924a7cbc7a5fba2ab3ea8084
Richard Vanbergen <r.vanbergen@cergis.com>
2015-12-02 10:38:38 +0000

    Tidying directive tests and adding new test.
    Now tests that the set attributes are removed.

--- specs/directive.js
+++ specs/directive.js
@@ -1,20 +1,6 @@
 describe('Unit: placeholderSizes directive.', function () {
   var _compile_, _rootScope_;
 
-  beforeEach(module('enrichit.ng-image-utils'));
-
-  beforeEach(inject(['$rootScope', '$compile', function ($rootScope, $compile) {
-    _compile_ = $compile;
-    _rootScope_ = $rootScope;
-  }]));
-
-  it('it exists and test framework is set up correctly', function () {
-    $elem = _compile_('<img placeholder-sizes ps-width="1024" ps-height="768" ng-src="http://lorempixel.com/1000/1000/" />')(_rootScope_);
-
-    expect($elem.attr('width')).toBe('1024');
-    expect($elem.attr('height')).toBe('768');
-  });
-
   function createPlaything(containerWidth) {
     var container = document.createElement('div');
 
@@ -32,25 +18,31 @@ describe('Unit: placeholderSizes directive.', function () {
 
     angular.element(document).find('body').append(container);
 
-    return container;
+    return _compile_(container)(_rootScope_);
   }
 
+  beforeEach(module('enrichit.ng-image-utils'));
+
+  beforeEach(inject(['$rootScope', '$compile', function ($rootScope, $compile) {
+    _compile_ = $compile;
+    _rootScope_ = $rootScope;
+  }]));
+
+  it('it exists and test framework is set up correctly', function () {
+    $elem = _compile_('<img placeholder-sizes ps-width="1024" ps-height="768" ng-src="http://lorempixel.com/1000/1000/" />')(_rootScope_);
+
+    expect($elem.attr('width')).toBe('1024');
+    expect($elem.attr('height')).toBe('768');
+  });
+
   it('doesn\'t resize when container is bigger than it', function () {
-    var plaything = createPlaything(2000);
-
-    var $elem = _compile_(plaything)(_rootScope_);
-    var $image = $elem.find('img');
-
+    var $image = createPlaything(2000).find('img');
     expect($image.attr('width')).toBe('1024');
     expect($image.attr('height')).toBe('768');
   });
 
   it('resizes when parent container is smaller than original height', function () {
-    var plaything = createPlaything(500);
-
-    var $elem = _compile_(plaything)(_rootScope_);
-    var $image = $elem.find('img');
-
+    var $image = createPlaything(500).find('img');
     expect($image.attr('width')).toBe('500');
     expect($image.attr('height')).toBe('375');
   });
@@ -59,5 +51,13 @@ describe('Unit: placeholderSizes directive.', function () {
     expect(function() {
       _compile_('<img placeholder-sizes ps-width="1024" ps-height="768" ng-src="http://lorempixel.com/1000/1000/" />')(_rootScope_);
     }).not.toThrow();
+  });
+
+  it('removes width and height after image loads', function () {
+    var $image = createPlaything(2000).find('img');
+    $image.triggerHandler('load');
+
+    expect($image.attr('width')).toBeUndefined();
+    expect($image.attr('height')).toBeUndefined();
   });
 })
